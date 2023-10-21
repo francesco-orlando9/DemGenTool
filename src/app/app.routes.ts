9fa3656f57c8ef6a4e4b2482d31b8d45e4de062e
francesco-orlando9 <francesco.orlando.dev@gmail.com>
2023-10-21 15:28:17 +0200

    remove ng start template + add page home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", pathMatch: "full", loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent) }
+];
